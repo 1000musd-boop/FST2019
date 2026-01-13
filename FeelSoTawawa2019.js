dbc7ca79cdb4b7d7b9c318ab306ab46894f5d5fc
1000musd-boop <1000musd@gmail.com>
2026-01-13 07:52:37 +0700

    Add files via upload

---
+++ FeelSoTawawa2019.js
@@ -0,0 +1,102 @@
+
+  <div class="server-item">
+
+<div class="separator" style="clear: both; text-align: center;"><img border="0" data-original-height="876" data-original-width="700" height="480" src="https://tv2.zonafilm.run/wp-content/uploads/2026/01/fXqL1TrURRPHMEkJNjXBwCI5Yan.jpg" width="300" /></div>
+    
+<br />
+     
+        <div class="server-item" style="position: relative; z-index: 1;">
+        <b>Informasi Film</b></div><div class="server-item" style="position: relative; z-index: 1;"><b><br />
+  Genre: Comedy, Drama
+          <br/>
+Kualitas: WEBDL
+        <br/>
+Tahun: 2019
+        <br/>
+Durasi: 79 Min
+        <br/>
+Negara:Japan
+        <br/>
+Rilis:12 Jul 2019
+        <br/>
+Bahasa:日本語
+        <br/>
+Direksi:Takeshi Furusawa
+        <br/>
+Pemain:Miu Akemi, Nanami Matsumoto, Tsubaki Kato      
+        
+        
+        </b></div>
+        
+        <div class="server-item" style="position: relative; z-index: 1;"><b><br />
+        Sinopsis Film.
+        <br/>
+  Film ini menceritakan Aya adalah seorang penulis erotis, tinggal bersama pacarnya, Hajime, seorang karyawan perusahaan. Belakangan ini, hubungan mereka agak renggang, dan Aya merasa agak tidak puas. Suatu hari, Aya mengetahui bahwa majalah tempat ia berkarya tiba-tiba akan berhenti terbit. Malam itu, ia diajak minum oleh Tanaka, seorang editor yang ia hormati, dan minum banyak hingga kehilangan kesadaran. Ketika bangun, ia mendapati Tanaka di ruangan lain sedang bercinta dengan seorang wanita misterius, Maria.      
+        
+        </b></div>
+ 
+<p style="text-align: center;"><b>&gt;&gt;&gt;&gt;&gt;=====&lt;&lt;&lt;&lt;&lt;</b></p><p style="text-align: center;"></p><h4 style="text-align: center;">
+  
+       </h4></div>
+       </div>
+<br />
+
+
+      <div class="iframe-container">
+
+  <marquee class="marquee" id="marquee"> * Trakteer dan Donasi klik di tombol bawah layar</marquee>
+  
+<iframe allowfullscreen="" frameborder="0" id="myIframe" mozallowfullscreen="" src="
+
+  https://www.youtube.com/embed/bUwIfvMwHws
+
+
+" webkitallowfullscreen=""></iframe>
+
+      <a class="tv2" href="https://trakteer.id/a_firstmed./tip " target="_blank">trakteer</a>
+      <a class="tv2" href="https://trakteer.id/a_firstmed. " target="_blank">Donasi</a>
+    </div>
+    
+
+  <br />
+ <!--<div class="server-item">
+<div class="club1" style="position: relative; z-index: 1;">-->
+        
+  <b>STREAMING MOVIE</b>
+
+          <br/>
+          
+         <button class="tv" onclick="changeChannel('https://www.youtube.com/embed/bUwIfvMwHws ')" target="search_iframe"><b><span>TRAILER</span></b></button> 
+          
+          <button class="tv" onclick="changeChannel(' https://filemoon.to/e/e5sv54i36sss/ ')"><b><span>SERVER 1</span></b></button> 
+          
+          <button class="tv" onclick="changeChannel('https://playerwish.com/e/z97cu1oef0gp ')" target="search_iframe"><b><span>SERVER 2</span></b></button> 
+      
+  <br />
+  
+          <button class="tv"><a href="https://gofile.io/d/Wz78eT " target="_blank"><b><span>DOWNLOAD 1</span></b></a></button>
+            
+          <button class="tv"><a href=" https://www.effectivegatecpm.com/g32hu1ezrk?key=1b18976a46999aa1d28e841622348709" target="_blank"><b><span>DOWNLOAD 2</span></b></a></button>
+                
+ <br />
+ <br />
+       
+       
+       
+       
+    
+          <script>
+        function changeChannel(url) {
+            document.getElementById('myIframe').src = url;
+        }
+       </script>
+  
+
+
+
+
+
+
+
+
+
